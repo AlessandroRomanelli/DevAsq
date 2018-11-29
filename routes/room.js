82632cd77fdfa145f10ee6bb2f6bb24fb6708609
Alessandro Romanelli <aleromrod@gmail.com>
2018-11-29 19:43:28 +0100

    Unique rooms

--- routes/room.js
+++ routes/room.js
@@ -7,18 +7,24 @@ router.get('/', (req, res) => {
     res.render('createRoom');
 });
 
-function Room(id, userId) {
+function Room(id, userId, name) {
     this.id = id;
-    this.userId = userId;
+    this.name = name;
+    this.creator = userId;
+    this.users = [userId];
+    this.publicPen = null;
+    this.checkers = [userId];
 }
 
 router.post('/create', (req, res) => {
     console.log(req.user);
     console.log(req.body);
     if (!req.user) return res.status(403).end();
-    const { roomId } = req.body;
+    const roomId = Math.random().toString().slice(2);
+    const { roomName } = req.body;
+    if (roomId in rooms) return res.status(400).end();
     const { _id } = req.user;
-    const room = new Room(roomId, _id);
+    const room = new Room(roomId, _id, roomName);
     rooms[roomId] = room;
     console.log(rooms);
     return res.status(201).end();
@@ -30,7 +36,7 @@ router.get('/join/:roomid', (req, res) => {
         res.status(400).end();
     }
     if (req.accepts('application/json')) {
-        res.json(roomId);
+        res.json(rooms[roomId]);
     }
     res.end();
 });
