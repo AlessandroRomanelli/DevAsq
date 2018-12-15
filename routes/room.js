f9525ad8588c6fccf29acc550b93ef7d06fae37d
Diego Carlino <diego_carlino@outlook.com>
2018-12-15 14:25:47 +0100

    bug fixed

--- routes/room.js
+++ routes/room.js
@@ -57,7 +57,7 @@ router.post('/join', (req, res) => {
 
 router.get('/:roomName', (req, res) => {
     const { roomName } = req.params;
-    if (!(roomName in roomStorage)) { return res.redirect('/'); }
+    if (!(roomName in roomStorage)) { return res.status(404).redirect('/'); }
     const room = roomStorage[req.params.roomName];
     if (!(room.hasUser(req.user._id))) return res.status(403).end();
     return res.render('pen', {
