9899ff7d36dd0c3b5b16ba44bc72dc3ec6e23d9f
Alessandro Romanelli <aleromrod@gmail.com>
2018-12-17 02:06:00 +0100

    Added favicon

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.engine('dust', kleiDust.dust);
 app.set('view engine', 'dust');
 
 // uncomment after placing your favicon in /public
-// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
